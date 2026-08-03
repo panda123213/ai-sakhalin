413cd2efa6a05b98968beec973bd8088a3572eaf
panda123213 <egorohov363@gmail.com>
2026-08-03 19:16:00 +1100

    День 7: завершил матрицы, выходной завтра

---
+++ stage2_math/matrix.py
@@ -0,0 +1,53 @@
+class Matrix:
+    def __init__(self,rows):
+        self.rows=rows
+        self.n_rows=len(rows)
+        self.n_cols=len(rows[0]) if rows else 0
+
+    def __repr__(self):
+        return '\n'.join(str(row) for row in self.rows)
+
+    def shape(self):
+        return(self.n_rows,self.n_cols)
+
+
+    def __add__(self, other):
+    # 1. Проверяем, что размеры матриц совпадают
+        if self.n_rows != other.n_rows or self.n_cols != other.n_cols:
+            raise ValueError("Матрицы должны быть одинакового размера")
+    
+    # 2. Складываем поэлементно
+        new_rows = []
+        for i in range(self.n_rows):
+            row = []
+            for j in range(self.n_cols):
+               row.append(self.rows[i][j] + other.rows[i][j])
+            new_rows.append(row)
+    
+        return Matrix(new_rows)
+
+    def mul_vec(self,vec):
+        if len(vec) != self.n_cols:
+            raise ValueError("Длина вектора не равна числу столбцов матрицы")
+        result = []
+        for row in self.rows:
+            total =0
+            for i in range(len(vec)):
+                total += row[i]*vec[i]
+            result.append(total)
+        return result
+
+m = Matrix([[1, 2], [3, 4]])
+v = [5, 6]
+print("Матрица:")
+print(m)
+print("Результат умножения на вектор:", m.mul_vec(v))
+    
+
+m1 = Matrix([[1, 2], [3, 4]])
+m2 = Matrix([[5, 6], [7, 8]])
+
+m3 = m1 + m2  # Теперь это работает!
+print(m3)
+
+        
